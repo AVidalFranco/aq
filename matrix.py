d110d9329452b72d51a2e68791930ce3c4abe045
AVidalFranco <antiavf@gmail.com>
2023-06-08 06:32:31 +0200

    proba2

--- matrix.py
+++ matrix.py
@@ -1,40 +1,64 @@
+import os
+import math
+import random
+import numpy as np
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
+import fitter as ft
+import pingouin as pg
+import statsmodels.api as sm 
 plt.style.use("ggplot")
-import os
 
 
-files=["BEN_pro.csv", "CO_pro.csv", "MXIL_pro.csv", "NO_pro.csv", "NO2_pro.csv", "O3_pro.csv", "PM2.5_pro.csv", "SO2_pro.csv", "TOL_pro.csv"]
+dfBEN = pd.read_csv('Database/BEN_pro.csv', delimiter=';')
+dfCO = pd.read_csv('Database/CO_pro.csv', delimiter=';')
+dfMXIL = pd.read_csv('Database/MXIL_pro.csv', delimiter=';')
+dfNO = pd.read_csv('Database/NO_pro.csv', delimiter=';')
+dfNO2 = pd.read_csv('Database/NO2_pro.csv', delimiter=';')
+dfO3 = pd.read_csv('Database/O3_pro.csv', delimiter=';')
+dfPM25 = pd.read_csv('Database/PM2.5_pro.csv', delimiter=';')
+dfSO2 = pd.read_csv('Database/SO2_pro.csv', delimiter=';')
+dfTOL = pd.read_csv('Database/TOL_pro.csv', delimiter=';')
 
-#data = pd.DataFrame(columns=["variable", "max", "min", "media", "mediana", "desviacion", "Q1", "Q2", "Q3"])
+# print (dfBEN)
 
-for f in files:
-    varname = f.split("_")
-    varname = varname[0]
+dfBEN.columns = ["date", "value", "year", "month", "day", "week", "startDate", "endDate", "weekOrder"]
+dfCO.columns = ["date", "value", "year", "month", "day", "week", "startDate", "endDate", "weekOrder"]
+dfMXIL.columns = ["date", "value", "year", "month", "day", "week", "startDate", "endDate", "weekOrder"]
+dfNO.columns = ["date", "value", "year", "month", "day", "week", "startDate", "endDate", "weekOrder"]
+dfNO2.columns = ["date", "value", "year", "month", "day", "week", "startDate", "endDate", "weekOrder"]
+dfO3.columns = ["date", "value", "year", "month", "day", "week", "startDate", "endDate", "weekOrder"]
+dfPM25.columns = ["date", "value", "year", "month", "day", "week", "startDate", "endDate", "weekOrder"]
+dfSO2.columns = ["date", "value", "year", "month", "day", "week", "startDate", "endDate", "weekOrder"]
+dfTOL.columns = ["date", "value", "year", "month", "day", "week", "startDate", "endDate", "weekOrder"]
 
-df = pd.read_csv(f"Database/{f}", sep=";", encoding="utf-8")
-df = df.apply(pd.to_numeric, errors="coerce")
+
+dfBEN = pd.to_numeric(dfBEN["value"], errors="coerce")
+dfBEN = dfBEN.iloc[:, 1:5]
+print(dfBEN)
+
+# corrBEN = dfBEN.corr()
+
+# print(corrBEN)
+
+# fig, ax = plt.subplots(figsize=(9,9))
+
+# ax = sns.heatmap(
+#     corr,
+#     vmin=-1, vmax=1, center=0,
+#     cmap=sns.diverging_palette(20,220, n=220),
+#     square=True
+# )
 
 
-#BENCENO
-ruta_arquivo = os.path.join("Database", "BEN_pro.csv")
-data = pd.read_csv(ruta_arquivo, delimiter=";") 
-corr = data.corr()
+# ax.set_xticklabels(
+#     ax.get_xticklabels(),
+#     rotation=45,
+#     horizontalalignment='right'
+# )
 
-plt.figure(figsize=(9,9))
 
-ax = sns.heatmap(
-    corr,
-    vmin=-1, vmax=1, center=0,
-    cmap=sns.diverging_palette(20, 220, n=200),
-    square=True
-)
-ax.set_xticklabels(
-    ax.get_xticklabels(),
-    rotation=45,
-    horizontalalignment='right'
-);
-
-plt.title("Matriz BENCENO")
-plt.show()
+# # plt.title("Matriz de correlación do {}".format(columns[0]))
+# plt.title("Matriz de correlación das variables")
+# plt.show() 
